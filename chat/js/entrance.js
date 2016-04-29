c6b2de44d346b64237094295cde02035285d97b0
Zhao <zhaoxb@sobot.com>
2016-04-29 09:41:45 +0800

    init

---
+++ chat/js/entrance.js
@@ -0,0 +1,22 @@
+(function(node){
+	
+	console.log(node);
+
+	var parseDOM = function(){
+	};
+
+	var bindListener = function(){
+	};
+
+	var initPlugins = function(){
+	};
+
+	var init = function(){
+		parseDOM();
+		bindListener();
+		initPlugins();
+	};
+
+	init();
+
+})(document.body);
---
+++ gulp/task/browerify.js
@@ -0,0 +1,10 @@
+var gulp = require("gulp");
+var browserify = require('gulp-browserify');
+var config = require('../config.json');
+gulp.task('browserify',function(){
+	gulp.src(config.baseDir+'js/**/entrance.js').
+	pipe(browserify({
+		'insertGlobals':false
+	})).
+	pipe(gulp.dest(config.baseDir+'dist/js'));
+});
