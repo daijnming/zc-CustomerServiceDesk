462248632ba52f060cdaa8dfbb3b986500b7fed4
Zhao <zhaoxb@sobot.com>
2016-05-05 19:15:17 +0800

    develop

--- chat/js/util/modal/dialog.js
+++ chat/js/util/modal/dialog.js
@@ -1,14 +1,57 @@
 /**
  * @author Treagzhao
  */
-function Dialog() {
-	var template = require('./template.js');
-	var layer = $(template.zcShadowLayer);
-	var show = function() {
-		$(document.body).append(layer);
-		console.log("aaaa");
-	};
-	this.show = show;
+function Dialog(spec) {
+    var template = require('./template.js');
+    var $layer,
+        $outer;
+    var conf = $.extend({
+        "okText" : "确定",
+        "title" : "提示"
+    },spec);
+    var initDOM = function() {
+        $layer = $(template.zcShadowLayer);
+        var _html = doT.template(template.zcModalOuter)(conf);
+        $outer = $(_html);
+    };
+
+    var setInner = function(elm) {
+        $outer.find(".modal-body").append($(elm));
+    };
+    var hide = function() {
+        $layer.find(".fade.in").animate({
+            'opacity' : 0
+        },300, function() {
+            setTimeout(function() {
+                $layer.remove();
+            },100);
+        });
+    };
+
+    var bindListener = function() {
+        $layer.on("click",hide);
+        $outer.on("click", function(evt) {
+            evt.stopPropagation();
+        });
+        $outer.delegate(".bootbox-close-button",'click',hide);
+        $outer.delegate(".js-cancel-btn","click", function(e) {
+            conf.cancel && conf.cancel();
+            hide();
+        });
+    };
+    var show = function() {
+        initDOM();
+        bindListener();
+        $layer.append($outer);
+        $(document.body).append($layer);
+        $layer.find(".fade.in").animate({
+            'opacity' : 1
+        },300);
+    };
+
+    this.setInner = setInner;
+    this.show = show;
+    this.hide = hide;
 }
 
 module.exports = Dialog;
--- chat/js/util/modal/template.js
+++ chat/js/util/modal/template.js
@@ -6,6 +6,32 @@ var template = {};
 var zcShadowLayer = '<div class="zc-shadow-layer">'+
 '</div>';
 
+var zcModalOuter = ''+
+'<div class="bootbox modal fade in" tabindex="-1" role="dialog" aria-hidden="false" style="display: block;">'+
+    '<div class="modal-dialog">'+
+        '<div class="modal-content">'+
+            '<div class="modal-header">'+
+                '<button type="button" class="bootbox-close-button close" data-dismiss="modal" aria-hidden="true">'+
+                    '×'+
+                '</button><h4 class="modal-title">{{=it.title || "提示"}}</h4>'+
+            '</div>'+
+            '<div class="modal-body">'+
+                
+            '</div>'+
+            '<div class="modal-footer">'+
+                '<button class="btn btn-default js-cancel-btn">'+
+                    '{{=it.cancelText || "取消"}}'+
+                '</button>'+
+                '<button class="btn btn-primary">'+
+                    '{{=it.okText || "确定"}}'+
+                '</button>'+
+            '</div>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
+'';
+
+template.zcModalOuter = zcModalOuter
 template.zcShadowLayer=zcShadowLayer;
 
 module.exports = template;
