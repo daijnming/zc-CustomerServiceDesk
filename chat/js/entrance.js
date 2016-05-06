060107ca898f3eb367ab7a23de739831d6903550
Zhao <zhaoxb@sobot.com>
2016-05-06 11:23:13 +0800

    develop

--- chat/js/entrance.js
+++ chat/js/entrance.js
@@ -1,34 +1,34 @@
 (function(node) {
-	var core = require('./Core/core.js')(window);
-	var LeftSide = require('./leftside/index.js');
-	var Promise = require('./util/promise.js');
-	var Modal = require('./util/modal/dialog.js');
-	var dialog = new Modal();
-	dialog.show();
-	var parseDOM = function() {
-	};
+    var core = require('./Core/core.js')(window);
+    var LeftSide = require('./leftside/index.js');
+    var Promise = require('./util/promise.js');
+    var Modal = require('./util/modal/dialog.js');
+    var Alert = require('./util/modal/alert.js');
 
-	var bindListener = function() {
-		$(window).on("resize", function(e) {
-			var height = ($(window).outerHeight());
-			$("#main-container").height($(window).outerHeight());
-			$("#chatlist").height(height);
-		});
-	};
+    var parseDOM = function() {
+    };
 
-	var initPlugins = function() {
-		var height = ($(window).outerHeight());
-		$("#main-container").height($(window).outerHeight());
-		$("#chatlist").height(height);
-		LeftSide($("section#left-navigation")[0],core,window);
-	};
+    var bindListener = function() {
+        $(window).on("resize", function(e) {
+            var height = ($(window).outerHeight());
+            $("#main-container").height($(window).outerHeight());
+            $("#chatlist").height(height);
+        });
+    };
 
-	var init = function() {
-		parseDOM();
-		bindListener();
-		initPlugins();
-	};
+    var initPlugins = function() {
+        var height = ($(window).outerHeight());
+        $("#main-container").height($(window).outerHeight());
+        $("#chatlist").height(height);
+        LeftSide($("section#left-navigation")[0],core,window);
+    };
 
-	init();
+    var init = function() {
+        parseDOM();
+        bindListener();
+        initPlugins();
+    };
+
+    init();
 
 })(document.body);
