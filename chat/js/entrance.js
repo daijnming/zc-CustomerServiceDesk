75e92e10307668348518644de054ecd4103064af
Zhao <zhaoxb@sobot.com>
2016-05-08 19:32:29 +0800

    develop

--- chat/js/entrance.js
+++ chat/js/entrance.js
@@ -1,24 +1,20 @@
 (function(node) {
-	var core = require('./Core/core.js')(window);
-	var LeftSide = require('./leftside/index.js');
-	var RightSide = require('./rightside/index.js');
-	var Promise = require('./util/promise.js');
-	var Modal = require('./util/modal/dialog.js');
-	var dialog = new Modal();
-	dialog.show();
-	var parseDOM = function() {
-	};
-
+    var core = require('./Core/core.js')(window);
+    var LeftSide = require('./leftside/index.js');
+    var RightSide = require('./rightside/index.js');
+    var Promise = require('./util/promise.js');
+    var Modal = require('./util/modal/dialog.js');
+    var fileLoader = require('./util/load.js')();
     var parseDOM = function() {
     };
 
-	var initPlugins = function() {
-		var height = ($(window).outerHeight());
-		$("#main-container").height($(window).outerHeight());
-		$("#chatlist").height(height);
-		LeftSide($("section#left-navigation")[0],core,window);
-		RightSide($('.rightBox'),core,window);
-	};
+    var initPlugins = function() {
+        var height = ($(window).outerHeight());
+        $("#main-container").height($(window).outerHeight());
+        $("#chatlist").height(height);
+        LeftSide($("section#left-navigation")[0],core,window);
+        RightSide($('.rightBox')[0],core,window);
+    };
     var bindListener = function() {
         $(window).on("resize", function(e) {
             var height = ($(window).outerHeight());
