b367f6b7c99f4e1346bb11ed1a51f49844dbc37a
Zhao <zhaoxb@sobot.com>
2016-05-08 19:55:45 +0800

    develop

--- chat/js/core/core.js
+++ chat/js/core/core.js
@@ -1,139 +1,140 @@
 function Core(window) {
-	var that = {};
-	var token = '';
-	var queryParam;
-	var polling = require('./socket/json.js');
-	var Promise = require('../util/promise.js');
-	var socket;
-	var global = {};
+    var that = {};
+    var token = '';
+    var queryParam;
+    var polling = require('./socket/json.js');
+    var Promise = require('../util/promise.js');
+    var socket;
+    var global = {};
 
-	var basicInfoHandler = function(value,promise) {
-		token = value.token || window.sessionStorage.getItem('temp-id');
-		promise.resolve({});
-	};
+    var basicInfoHandler = function(value,promise) {
+        token = value.token || window.sessionStorage.getItem('temp-id');
+        promise.resolve({});
+    };
 
-	/**
-	 * 将url里面query字符串转换成对象
-	 */
-	var getQueryParam = function() {
-		var href = location.href;
-		var queryString = href.substring(href.lastIndexOf("?") + 1);
-		if(queryString.lastIndexOf("#") >= 0) {
-			queryString = queryString.substring(0,queryString.lastIndexOf("#"));
-		}
-		var list = queryString.split("&");
-		var param = {};
-		for(var i = 0;i < list.length;i++) {
-			var item = list[i];
-			var key = item.substring(0,item.indexOf("="));
-			var value = item.substring(item.indexOf("=") + 1);
-			if(/^-?(\d+)(\.\d+)?$/.test(value)) {
-				param[key] = Number(value);
-			} else if(value === 'true') {
-				param[key] = true;
-			} else if(value === 'false') {
-				param[key] = false;
-			} else {
-				param[key] = value;
-			}
-		}
-		return param;
-	};
+    /**
+     * 将url里面query字符串转换成对象
+     */
+    var getQueryParam = function() {
+        var href = location.href;
+        var queryString = href.substring(href.lastIndexOf("?") + 1);
+        if(queryString.lastIndexOf("#") >= 0) {
+            queryString = queryString.substring(0,queryString.lastIndexOf("#"));
+        }
+        var list = queryString.split("&");
+        var param = {};
+        for(var i = 0;i < list.length;i++) {
+            var item = list[i];
+            var key = item.substring(0,item.indexOf("="));
+            var value = item.substring(item.indexOf("=") + 1);
+            if(/^-?(\d+)(\.\d+)?$/.test(value)) {
+                param[key] = Number(value);
+            } else if(value === 'true') {
+                param[key] = true;
+            } else if(value === 'false') {
+                param[key] = false;
+            } else {
+                param[key] = value;
+            }
+        }
+        return param;
+    };
 
-	var getMessage = function() {
-		socket.start();
-	};
+    var getMessage = function() {
+        socket.start();
+    };
 
-	var initBasicInfo = function() {
-		Promise.when(function() {
-			var promise = new Promise();
-			$.ajax({
-				'url' : 'getEnvironment',
-				'data' : {
-				},
-				'dataType' : 'json'
-			}).success(function(ret) {
-				promise.resolve({
-					'token' : ret.token
-				});
-			}).fail(function(ret) {
-				promise.reject({
-					'token' : null
-				});
-			});
-			return promise;
-		}).then(basicInfoHandler,basicInfoHandler).then(function(value,promise) {
-			$.ajax({
-				'url' : '/chat/admin/connect.action',
-				'dataType' : 'json',
-				'type' : 'get',
-				'data' : {
-					'uid' : queryParam.id,
-					'way' : 1,
-					'st' : queryParam.st || 1,
-					'lt' : queryParam.lt || new Date().getTime(),
-					'token' : token
-				}
-			}).done(function(ret) {
-				if(ret.status == 1) {
-					for(var el in ret) {
-						global[el] = ret[el];
-					}
-					$(".js-loading-layer").hide();
-					promise.resolve(ret);
-				} else if(ret.status == -2) {
-					alert('登录失败');
-					window.close();
-				}
-			});
-		}).then(function(value,promise) {
-			$(document.body).trigger("core.onload",[global]);
-			getMessage();
-		});
-	};
+    var initBasicInfo = function() {
+        Promise.when(function() {
+            var promise = new Promise();
+            $.ajax({
+                'url' : 'getEnvironment',
+                'data' : {
+                },
+                'dataType' : 'json'
+            }).success(function(ret) {
+                promise.resolve({
+                    'token' : ret.token
+                });
+            }).fail(function(ret) {
+                promise.reject({
+                    'token' : null
+                });
+            });
+            return promise;
+        }).then(basicInfoHandler,basicInfoHandler).then(function(value,promise) {
+            $.ajax({
+                'url' : '/chat/admin/connect.action',
+                'dataType' : 'json',
+                'type' : 'get',
+                'data' : {
+                    'uid' : queryParam.id,
+                    'way' : 1,
+                    'st' : queryParam.st || 1,
+                    'lt' : queryParam.lt || new Date().getTime(),
+                    'token' : token
+                }
+            }).done(function(ret) {
+                if(ret.status == 1) {
+                    for(var el in ret) {
+                        global[el] = ret[el];
+                    }
+                    global.baseUrl = location.protocol + "//" + location.host + "/chat/";
+                    $(".js-loading-layer").hide();
+                    promise.resolve(ret);
+                } else if(ret.status == -2) {
+                    alert('登录失败');
+                    window.close();
+                }
+            });
+        }).then(function(value,promise) {
+            $(document.body).trigger("core.onload",[global]);
+            getMessage();
+        });
+    };
 
-	var getGlobal = function() {
-		return global;
-	};
+    var getGlobal = function() {
+        return global;
+    };
 
-	var parseDOM = function() {
-	};
+    var parseDOM = function() {
+    };
 
-	var bindListener = function() {
-	};
+    var bindListener = function() {
+    };
 
-	var socketFactory = function() {
-		if(window.WebSocket && false) {
+    var socketFactory = function() {
+        if(window.WebSocket && false) {
 
-		} else {
-			socket = new polling(global);
-		}
+        } else {
+            socket = new polling(global);
+        }
 
-		socket.on("receive", function(value) {
-			$(document.body).trigger('core.receive',value);
-		});
-	};
+        socket.on("receive", function(value) {
+            $(document.body).trigger('core.receive',value);
+        });
+    };
 
-	var initPlugins = function() {
-		queryParam = getQueryParam();
-		for(var el in queryParam) {
-			global[el] = queryParam[el];
-		}
-		initBasicInfo();
-		socketFactory();
-	};
+    var initPlugins = function() {
+        queryParam = getQueryParam();
+        for(var el in queryParam) {
+            global[el] = queryParam[el];
+        }
+        initBasicInfo();
+        socketFactory();
+    };
 
-	var init = function() {
-		parseDOM();
-		bindListener();
-		initPlugins();
-	};
+    var init = function() {
+        parseDOM();
+        bindListener();
+        initPlugins();
+    };
 
-	init();
+    init();
 
-	that.getQueryParam = getQueryParam;
-	that.getGlobal = getGlobal;
-	return that;
+    that.getQueryParam = getQueryParam;
+    that.getGlobal = getGlobal;
+    return that;
 }
 
 module.exports = Core;
--- chat/js/leftside/index.js
+++ chat/js/leftside/index.js
@@ -1,5 +1,7 @@
 function LeftSide(node,core,window) {
     var template = require('./template.js');
+    var loadFile = require('../util/load.js')();
+    var global;
     var parseDOM = function() {
 
     };
@@ -10,13 +12,38 @@ function LeftSide(node,core,window) {
         $(node).find(".js-users-list").append(li);
     };
     var onReceive = function(value,data) {
-        if(data.type == 102) {
-            newUserMessage(data);
+        switch(data.type) {
+            case 102:
+                newUserMessage(data);
+                break;
         }
     };
+
+    var getDefaultChatList = function() {
+        console.log(global);
+        $.ajax({
+            'url' : '/chat/admin/getAdminChats.action',
+            'dataType' : 'json',
+            'type' : 'get',
+            'data' : {
+                'uid' : global.id
+            }
+        }).success(function(ret) {
+            console.log(ret);
+            loadFile.load(global.baseUrl + 'views/leftside/chatlist.html').then(function(value) {
+                var _html = doT.template(value)({
+                    'list' : ret.userList
+                });
+                $(node).find(".js-users-list").html(_html);
+            });
+        });
+    };
+
     var onloadHandler = function(evt,data) {
+        global = core.getGlobal();
         $(node).find("img.js-my-logo").attr("src",data.face);
         $(node).find(".js-customer-service").html(data.name);
+        getDefaultChatList();
     };
 
     var bindLitener = function() {
