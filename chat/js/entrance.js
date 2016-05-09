7351da6c3d35c4c75fb51418a024d676373eb69c
GoblinStar <testlushi2@163.com>
2016-05-09 16:55:12 +0800

    init chatlist

--- chat/js/entrance.js
+++ chat/js/entrance.js
@@ -1,7 +1,8 @@
 (function(node) {
     var core = require('./Core/core.js')(window);
     var LeftSide = require('./leftside/index.js');
-    var RightSide = require('./rightside/index.js');
+    // var RightSide = require('./rightside/index.js');
+    var ChatList = require('./chatlist/index.js');
     var Promise = require('./util/promise.js');
     var Modal = require('./util/modal/dialog.js');
     var fileLoader = require('./util/load.js')();
@@ -13,7 +14,8 @@
         $("#main-container").height($(window).outerHeight());
         $("#chatlist").height(height);
         LeftSide($("section#left-navigation")[0],core,window);
-        RightSide($('.rightBox')[0],core,window);
+        // RightSide($('.rightBox')[0],core,window);
+        ChatList($('#chatlist'), core, window);
     };
     var bindListener = function() {
         $(window).on("resize", function(e) {
