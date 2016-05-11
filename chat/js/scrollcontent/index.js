28851f98e00de2bbb0e4d539b2af1b0d766c413b
Zhao <zhaoxb@sobot.com>
2016-05-11 11:04:53 +0800

    develop

--- chat/js/scrollcontent/index.js
+++ chat/js/scrollcontent/index.js
@@ -5,149 +5,145 @@ function Content(node,core,window) {
     // 保存用户对话消息缓存
     var chatCache = {};
     var API = {
-      http: {
-        chatList: {
-          'chat': 'admin/get_chatdetail.action',
-          'hchat': 'admin/get_chatdetail.action'
+        http : {
+            chatList : {
+                'chat' : 'admin/get_chatdetail.action',
+                'hchat' : 'admin/get_chatdetail.action'
+            },
+
+            status : {
+                black : {
+                    add : 'admin/add_blacklist.action',
+                    del : 'admin/delete_blacklist.action'
+                },
+
+                star : {
+                    add : 'admin/add_marklist.action',
+                    del : 'admin/delete_marklist.action'
+                }
+            },
+
+            addTransfer : ''
         },
 
-        status: {
-          black: {
-            add: 'admin/add_blacklist.action' ,
-            del: 'admin/delete_blacklist.action'
-          },
-
-          star: {
-            add: 'admin/add_marklist.action' ,
-            del: 'admin/delete_marklist.action'
-          }
-        },
-
-        addTransfer: ''
-      },
-
-      tpl: {
-        chatList: 'views/scrollcontent/list.html' ,
-        chatItem: 'views/scrollcontent/item.html' ,
-        chatItemByAdmin: 'views/scrollcontent/itemByAdmin.html'
-      }
+        tpl : {
+            chatList : 'views/scrollcontent/list.html',
+            chatItem : 'views/scrollcontent/item.html',
+            chatItemByAdmin : 'views/scrollcontent/itemByAdmin.html'
+        }
     };
 
     var userSourceMap = {
-      0: 'http://img.sobot.com/chatres/common/face/pcType.png' ,
-      1: 'http://img.sobot.com/chatres/common/face/weixinType.png' ,
-      2: 'http://img.sobot.com/chatres/common/face/appType.png' ,
-      3: 'http://img.sobot.com/chatres/common/face/weiboType.png' ,
-      4: 'http://img.sobot.com/chatres/common/face/moType.png' ,
-      5: 'http://img.sobot.com/chatres/common/face/moType.png'
+        0 : 'http://img.sobot.com/chatres/common/face/pcType.png',
+        1 : 'http://img.sobot.com/chatres/common/face/weixinType.png',
+        2 : 'http://img.sobot.com/chatres/common/face/appType.png',
+        3 : 'http://img.sobot.com/chatres/common/face/weiboType.png',
+        4 : 'http://img.sobot.com/chatres/common/face/moType.png',
+        5 : 'http://img.sobot.com/chatres/common/face/moType.png'
     }
 
     var userInfo = {
-      uid: '088ad376b6514ed0a191067308c284fe000025' ,
-      pid: '088ad376b6514ed0a191067308c284fe' ,
-      sender: 'mTLX96VS13KSmF6VqxK9KBtavHt7fYVcV6Ekx3YuXvcoNskGFZk2xQ==' ,
-      isStar: true,
-      isBlack: true
+        uid : '088ad376b6514ed0a191067308c284fe000025',
+        pid : '088ad376b6514ed0a191067308c284fe',
+        sender : 'mTLX96VS13KSmF6VqxK9KBtavHt7fYVcV6Ekx3YuXvcoNskGFZk2xQ==',
+        isStar : true,
+        isBlack : true
     }
 
     // --------------------------- http 请求 ---------------------------
 
     // 加载
-    var getChatListByOnline = function(type, callback) {
+    var getChatListByOnline = function(type,callback) {
 
-      if (typeof arguments[0] === 'function') {
-        callback = arguments[0];
-        type = 'chat';
-      }18
+        if( typeof arguments[0] === 'function') {
+            callback = arguments[0];
+            type = 'chat';
+        }18
 
-      $.ajax({
-          'url' : API.http.chatList[type],
-          'dataType' : 'json',
-          'type' : 'get',
-          'data' : {
-              t: '1462871390676' ,
-              uid: userInfo.uid ,
-              pid: userInfo.pid ,
-              pageNow: 1 ,
-              pageSize: 20
-          }
-      }).success(function(ret) {
-        callback && callback(type, ret);
-      })
+        $.ajax({
+            'url' : API.http.chatList[type],
+            'dataType' : 'json',
+            'type' : 'get',
+            'data' : {
+                t : '1462871390676',
+                uid : userInfo.uid,
+                pid : userInfo.pid,
+                pageNow : 1,
+                pageSize : 20
+            }
+        }).success(function(ret) {
+            callback && callback(type,ret);
+        })
     };
 
-    var updateUserState = function(type, handleType, callback) {
-      console.log('init updateUserState');
-      console.log(arguments);
-      console.log(API.http.status[type][handleType])
-      $.ajax({
-          'url' : API.http.status[type][handleType],
-          'dataType' : 'json',
-          'type' : 'get',
-          'data' : {
-            sender: userInfo.sender ,
-            receiver: userInfo.uid
-          }
-      }).success(function(ret) {
-        console.log(ret);
-        callback && callback(type, handleType);
-      });
+    var updateUserState = function(type,handleType,callback) {
+        console.log('init updateUserState');
+        console.log(arguments);
+        console.log(API.http.status[type][handleType])
+        $.ajax({
+            'url' : API.http.status[type][handleType],
+            'dataType' : 'json',
+            'type' : 'get',
+            'data' : {
+                sender : userInfo.sender,
+                receiver : userInfo.uid
+            }
+        }).success(function(ret) {
+            console.log(ret);
+            callback && callback(type,handleType);
+        });
     }
-
     // --------------------------- dom操作 ---------------------------
 
-    var parseTpl = function(type, ret) {
-      loadFile.load(global.baseUrl + API.tpl.chatList)
-      .then(function(tpl) {
+    var parseTpl = function(type,ret) {
+        loadFile.load(global.baseUrl + API.tpl.chatList).then(function(tpl) {
 
-        var list = []
-          , _html ;
+            var list = [],
+                _html;
 
-        ret.data.map(function(item){
-          list.push({
-            action: 'dateline' ,
-            date: item.date
-          });
+            ret.data.map(function(item) {
+                list.push({
+                    action : 'dateline',
+                    date : item.date
+                });
 
-          item.content.map(function(obj){
-            // obj.userHeadImage = userSourceMap[obj.source];
-            list.push(obj);
-          });
+                item.content.map(function(obj) {
+                    // obj.userHeadImage = userSourceMap[obj.source];
+                    list.push(obj);
+                });
+            });
+
+            _html = doT.template(tpl)({
+                list : list
+            });
+
+            $rootNode.find('#' + type).find('.js-panel-body').html(_html);
         });
-
-        _html = doT.template(tpl)({
-          list: list
-        });
-
-        $rootNode.find('#' + type).find('.js-panel-body').html(_html);
-      });
     }
-
-    var updateHeaderTag = function(type, handleType) {
-      $rootNode.find('.js-addButton').children('[data-type="' + type + '"]').removeClass('hide');
-      $rootNode.find('.js-addButton').children('.js-' + type + '-' + handleType).addClass('hide');
+    var updateHeaderTag = function(type,handleType) {
+        $rootNode.find('.js-addButton').children('[data-type="' + type + '"]').removeClass('hide');
+        $rootNode.find('.js-addButton').children('.js-' + type + '-' + handleType).addClass('hide');
     }
-
     var initUserState = function(data) {
-      $rootNode.find('.js-addButton').children('a').addClass('hide');
+        $rootNode.find('.js-addButton').children('a').addClass('hide');
 
-      for (var k in data) $rootNode.find('.js-addButton').children('.js-' + k + '-' + ( data[k] ? 'del' : 'add') ).removeClass('hide');
+        for(var k in data)
+        $rootNode.find('.js-addButton').children('.js-' + k + '-' + (data[k] ? 'del' : 'add')).removeClass('hide');
     }
-
     var parseChat = {
-      102: function(data) {
-        data.userHeadImage = userSourceMap[data.source];
-        data.t = new Date(data.t).toLocaleString().split(' ')[1];
-      },
+        102 : function(data) {
+            data.userHeadImage = userSourceMap[data.source];
+            data.t = new Date(data.t).toLocaleString().split(' ')[1];
+        },
 
-      103: function(data){
-        data.userHeadImage = userSourceMap[data.source];
-        data.ts = data.ts.split(' ')[1];
-      },
+        103 : function(data) {
+            data.userHeadImage = userSourceMap[data.source];
+            data.ts = data.ts.split(' ')[1];
+        },
 
-      108: function(data) {
-        data.t = new Date(data.t).toLocaleString().split(' ')[1];
-      }
+        108 : function(data) {
+            data.t = new Date(data.t).toLocaleString().split(' ')[1];
+        }
     }
 
     // --------------------------- socket ---------------------------
@@ -155,41 +151,38 @@ function Content(node,core,window) {
     var userPushMessage = function(data) {
         parseChat[data.type] && parseChat[data.type](data);
         // 用户发送消息
-        loadFile.load(global.baseUrl + API.tpl.chatItem)
-        .then(function(tpl) {
-          var _html;
-          _html = doT.template(tpl)({
-            data: data
-          });
+        loadFile.load(global.baseUrl + API.tpl.chatItem).then(function(tpl) {
+            var _html;
+            _html = doT.template(tpl)({
+                data : data
+            });
 
-          $rootNode.find('#chat').find('.js-panel-body').append(_html);
+            $rootNode.find('#chat').find('.js-panel-body').append(_html);
         });
     };
 
     var adminPushMessage = function(data) {
 
         // 客服发送消息
-        loadFile.load(global.baseUrl + API.tpl.chatItem)
-        .then(function(tpl) {
-          var _html;
-          _html = doT.template(tpl)({
-            data: data
-          });
+        loadFile.load(global.baseUrl + API.tpl.chatItem).then(function(tpl) {
+            var _html;
+            _html = doT.template(tpl)({
+                data : data
+            });
 
-          $rootNode.find('#chat').find('.js-panel-body').append(_html);
+            $rootNode.find('#chat').find('.js-panel-body').append(_html);
         });
     }
-
     // --------------------------- base ---------------------------
 
     var parseDOM = function() {
-      $rootNode.find('.js-addButton').on('click', '.js-goOut', function(event){
-        var $self = $(this);
-        var type = $self.attr('data-type');
-        var handleType = $self.attr('data-handle');
-        console.log(type, handleType);
-        updateUserState(type, handleType, updateHeaderTag);
-      })
+        $rootNode.find('.js-addButton').on('click','.js-goOut', function(event) {
+            var $self = $(this);
+            var type = $self.attr('data-type');
+            var handleType = $self.attr('data-handle');
+            console.log(type,handleType);
+            updateUserState(type,handleType,updateHeaderTag);
+        })
     };
 
     var onReceive = function(value,data) {
@@ -205,8 +198,8 @@ function Content(node,core,window) {
 
         // 初始化用户状态
         initUserState({
-          star: userInfo.isStar ,
-          black: userInfo.isBlack ,
+            star : userInfo.isStar,
+            black : userInfo.isBlack ,
         });
         // updateUserState('star', 'add', updateHeaderTag);
     };
--- chat/js/textarea/index.js
+++ chat/js/textarea/index.js
@@ -1,17 +1,20 @@
-function TextArea(node,core,window){
-	var loadFile = require('../util/load.js')();
-	var global = core.getGlobal();
-	var baseUrl = global.baseUrl;
-	var uid="daijm";
-	var showMsg=require('./showMsg.js');//会话气泡
-	var ZC_Face=require('../util/qqFace.js');//表情
-	var AjaxUpload=require('../util/upload.js');//上传附件
-	var $node;
-	console.log(showMsg);
-	//var apihost = "http://test.sobot.com/chat/";
-	//var sendurl = apihost+"admin/send1.action"
+function TextArea(node,core,window) {
+    var loadFile = require('../util/load.js')();
+    var global = core.getGlobal();
+    var baseUrl = global.baseUrl;
+    var uid = "daijm";
+    var showMsg = require('./showMsg.js');
+    //会话气泡
+    var ZC_Face = require('../util/qqFace.js');
+    //表情
+    var AjaxUpload = require('../util/upload.js');
+    //上传附件
+    var $node;
+    console.log(showMsg);
+    //var apihost = "http://test.sobot.com/chat/";
+    //var sendurl = apihost+"admin/send1.action"
     var parseDOM = function() {
-    	$node=$(node);
+        $node = $(node);
     };
     var newUserMessage = function(data) {
         var _html = doT.template(template.listItem)(data);
@@ -49,7 +52,7 @@ function TextArea(node,core,window){
     };
 
     var onloadHandler = function(evt,data) {
-        
+
         $node.find("img.js-my-logo").attr("src",data.face);
         $node.find(".js-customer-service").html(data.name);
         getDefaultChatList();
@@ -60,191 +63,193 @@ function TextArea(node,core,window){
         $(document.body).on("core.receive",onReceive);
 
         /*
-		*qq表情
-        */
-          	  $node.find(".js-emotion").click(function(){
-			  	//打开集合,默认qq表情为显示状态
-			  	$node.find("#faceGroup").show();
-			  	$node.find("#emojiGroup").hide();
-			 	ZC_Face.show();
-			 	ZC_Face.emojiShow();
-			  });
-			  $node.find(".btnSend").click(function(){//发送btn
-					var str = $node.find(".js-sendMessage").val();
-					str=ZC_Face.analysis(str);//str已做表情处理
-					$node.find(".js-sendMessage").val("");//清空待发送框
-					showMsg(uid,"daijm","img/qqarclist/jianjiao.gif",str,null,null,null);//显示气泡
-				 })
-			  $node.find(".icoLi").click(function(){
-			  	$(this).addClass("active").siblings().removeClass("active");
-			  	$node.find('.groupChildren').hide();
-			  	var dataId=$(this).attr("data-src");
-			  	$(dataId).show();
-			  })
-
-
+         *qq表情
+         */
+        $node.find(".js-emotion").click(function() {
+            //打开集合,默认qq表情为显示状态
+            $node.find("#faceGroup").show();
+            $node.find("#emojiGroup").hide();
+            ZC_Face.show();
+            ZC_Face.emojiShow();
+        });
+        $node.find(".btnSend").click(function() {//发送btn
+            var str = $node.find(".js-sendMessage").val();
+            str = ZC_Face.analysis(str);
+            //str已做表情处理
+            $node.find(".js-sendMessage").val("");
+            //清空待发送框
+            showMsg(uid,"daijm","img/qqarclist/jianjiao.gif",str,null,null,null);
+            //显示气泡
+        })
+        $node.find(".icoLi").click(function() {
+            $(this).addClass("active").siblings().removeClass("active");
+            $node.find('.groupChildren').hide();
+            var dataId = $(this).attr("data-src");
+            $(dataId).show();
+        })
     };
 
     var initPlugsin = function() {//插件
-		global = core.getGlobal();
-		initFace();
-		uploadFile();
+        global = core.getGlobal();
+        initFace();
+        uploadFile();
     };
 
-	var initFace = function(){
-		/*
-		*saytext 待发送内容框
-		*group 大表情集合
-		*faceGroup表情集合
-		*emojiGroup emoji表情集合
-		*showId	聊天窗体
-		*emotion 表情集合按钮
-		*sub_btn 提交按钮
-		*path 表情集合路径
-		*emojiPath emoji表情集合路径
-		*/
-		ZC_Face.initConfig({
-		 	saytext:".js-sendMessage",
-		 	Group:"#faceGroupTarea",
-		 	faceGroup:"#faceGroup",
-		 	emojiGroup:"#emojiGroup",
-		 	showId:".panel-body",
-		 	emotion:".js-emotion",
-		 	sub_btn:".btnSend",
-		 	path:"img/qqarclist/",
-		 	emojiPath:"img/emoji/"
-		},function(){
-			//cbk
-		});
-		 
-		//$node.find('.face').perfectScrollbar();//加载滚动条
-	};
-	var uploadFile=function(){
-		var uploadBtn=$node.find(".jsUpload");//btn
-	  
-		uploadImg(uploadBtn);//对准btn
-	  
-	 	function uploadImg(uploadBtn){
-			
-			var uploadOption = {//上传附件
-		        action: /*apihost+*/"webchat/fileupload.action",
-		        name: "file",
-		        autoSubmit: true,
-		        data:{
-		          type: "msg",
-		          countTag: 0
-		        },
-		        responseType:"JSONP",
-		        contentType:"application/x-www-form-urlencoded; charset=utf-8",
-		        onChange: function (file, extension){//file文件名称，
-		        	 
-		        	//if(source==0){
-			        	if (!(extension && /^(jpg|JPG|png|PNG|gif|GIF|txt|TXT|DOC|doc|docx|DOCX|pdf|PDF|ppt|PPT|pptx|PPTX|xls|XLS|xlsx|XLSX|RAR|rar|zip|ZIP)$/.test(extension))) {//|mp3|MP3|mp4|MP4|wma|WMA|wmv|WMV|rmvb|RMVB
-			        		 $.amaran({
-			        	            content:{
-			        	                message:'格式不支持!',
-			        	                size:'请上传正确的文件格式',
-			        	                file:'',
-			        	                icon:'fa fa-times'
-			        	            },
-			        	            theme:'default error',
-			        	            position:'bottom right',
-			        	            inEffect:'slideRight',
-			        	            outEffect:'slideBottom'
-			        	       });
-			                return false;
-			            }
-		        	/*}else{
-		        		if (!(extension && /^(jpg|JPG|png|PNG|gif|GIF)$/.test(extension))) {
-			        		 $.amaran({
-			        	            content:{
-			        	                message:'图片格式不支持!',
-			        	                size:'请上传jpg/png/gif格式图片',
-			        	                file:'',
-			        	                icon:'fa fa-times'
-			        	            },
-			        	            theme:'default error',
-			        	            position:'bottom right',
-			        	            inEffect:'slideRight',
-			        	            outEffect:'slideBottom'
-			        	       });
-			                return false;
-			            }*/
-			    
-		    
-			    },
-			    onSubmit: function (file, extension, base64file){
-			    	var fileTypeReg = /\.(gif|jpg|jpeg|png|GIF|JPG|PNG)$/;
-				 
-			          //uid = $("[class='user active']").attr('uid')
-			         // cid = $("[class='user active']").attr('cid');
-			         // usource = $("[class='user active']").attr('usource');
+    var initFace = function() {
+        /*
+         *saytext 待发送内容框
+         *group 大表情集合
+         *faceGroup表情集合
+         *emojiGroup emoji表情集合
+         *showId	聊天窗体
+         *emotion 表情集合按钮
+         *sub_btn 提交按钮
+         *path 表情集合路径
+         *emojiPath emoji表情集合路径
+         */
+        ZC_Face.initConfig({
+            saytext : ".js-sendMessage",
+            Group : "#faceGroupTarea",
+            faceGroup : "#faceGroup",
+            emojiGroup : "#emojiGroup",
+            showId : ".panel-body",
+            emotion : ".js-emotion",
+            sub_btn : ".btnSend",
+            path : "img/qqarclist/",
+            emojiPath : "img/emoji/"
+        }, function() {
+            //cbk
+        });
 
-			         // Global.map[$("[class='user active']").attr('uid')].count = Global.map[$("[class='user active']").attr('uid')].count || 0;
+        //$node.find('.face').perfectScrollbar();//加载滚动条
+    };
+    var uploadFile = function() {
+        var uploadBtn = $node.find(".jsUpload");
+        //btn
 
-			        //  if (fileTypeReg.test(file)) Global.map[$("[class='user active']").attr('uid')].count++;
-			        //  uploadOption.data.source = Global.usource;
-			        //  uploadOption.data.countTag = Global.map[uid].count;
+        uploadImg(uploadBtn);
+        //对准btn
 
-			        	if(extension && /^(jpg|JPG|png|PNG|gif|GIF)$/.test(extension))
-			        	{console.log(1);
-							//去除admin/
-			        		//var con = '<img src="img/upImgLoad.png" class="webchat_img_upload upNowImg">';
-			        		 
-			        	showMsg(uid,"daijm","img/qqarclist/jianjiao.gif","",null,"<img src='img/appType.png'>",base64file);//显示气泡
-			        	}
-			        	//else
-			        	//{console.log(2);
-			        	//	var oMsgDom = '<div class="systeamTextBox systeamNowText"><p class="systeamText">正在上传 '+ file +'</p></div>';
-			        	//	$node.find(".panel-body").append(oMsgDom);
-			        		//$node.find('.scrollBoxParent').scrollTop(999999);
-			        	//}
-			    },
-			    onComplete: function (file,response) {
-					 var countTag = 0,
-			              res,
-			              url, 
-			              size;
+        function uploadImg(uploadBtn) {
 
-			    			if (typeof response == 'string') {
-			              res = JSON.parse(response);
-			              url = res.url;
-			    			    countTag = parseInt(res.countTag) - 1;
-			    			}else{
-			    				  url = response.url;
-			    			}
+            var uploadOption = {//上传附件
+                action : /*apihost+*/"webchat/fileupload.action",
+                name : "file",
+                autoSubmit : true,
+                data : {
+                    type : "msg",
+                    countTag : 0
+                },
+                responseType : "JSONP",
+                contentType : "application/x-www-form-urlencoded; charset=utf-8",
+                onChange : function(file,extension) {//file文件名称，
 
-			        	size = response.filesize;
+                    //if(source==0){
+                    if(!(extension && /^(jpg|JPG|png|PNG|gif|GIF|txt|TXT|DOC|doc|docx|DOCX|pdf|PDF|ppt|PPT|pptx|PPTX|xls|XLS|xlsx|XLSX|RAR|rar|zip|ZIP)$/.test(extension))) {//|mp3|MP3|mp4|MP4|wma|WMA|wmv|WMV|rmvb|RMVB
+                        $.amaran({
+                            content : {
+                                message : '格式不支持!',
+                                size : '请上传正确的文件格式',
+                                file : '',
+                                icon : 'fa fa-times'
+                            },
+                            theme : 'default error',
+                            position : 'bottom right',
+                            inEffect : 'slideRight',
+                            outEffect : 'slideBottom'
+                        });
+                        return false;
+                    }
+                    /*}else{
+                     if (!(extension && /^(jpg|JPG|png|PNG|gif|GIF)$/.test(extension))) {
+                     $.amaran({
+                     content:{
+                     message:'图片格式不支持!',
+                     size:'请上传jpg/png/gif格式图片',
+                     file:'',
+                     icon:'fa fa-times'
+                     },
+                     theme:'default error',
+                     position:'bottom right',
+                     inEffect:'slideRight',
+                     outEffect:'slideBottom'
+                     });
+                     return false;
+                     }*/
 
-			        	if (size==false) {
-			        		 $.amaran({
-			     	            content:{
-			     	                message:'所传文件过大!',
-			     	                size:'',
-			     	                file:'',
-			     	                icon:'fa fa-times'
-			     	            },
-			     	            theme:'default error',
-			     	            position:'bottom right',
-			     	            inEffect:'slideRight',
-			     	            outEffect:'slideBottom'
-			     	       });
-			        		 return;
-			        	 }
+                },
+                onSubmit : function(file,extension,base64file) {
+                    var fileTypeReg = /\.(gif|jpg|jpeg|png|GIF|JPG|PNG)$/;
 
-			        	 //imgCallBack(uid, url, cid, countTag);
-			      
-			    }    
-		    
-			}
-			/*
-			*uploadBtn 附件按钮
-			*uploadOption 上传参数
-			*/
-			 new AjaxUpload(uploadBtn, uploadOption);
-		}
-		
-	};
+                    //uid = $("[class='user active']").attr('uid')
+                    // cid = $("[class='user active']").attr('cid');
+                    // usource = $("[class='user active']").attr('usource');
+
+                    // Global.map[$("[class='user active']").attr('uid')].count = Global.map[$("[class='user active']").attr('uid')].count || 0;
+
+                    //  if (fileTypeReg.test(file)) Global.map[$("[class='user active']").attr('uid')].count++;
+                    //  uploadOption.data.source = Global.usource;
+                    //  uploadOption.data.countTag = Global.map[uid].count;
+
+                    if(extension && /^(jpg|JPG|png|PNG|gif|GIF)$/.test(extension)) {
+                        console.log(1);
+                        //去除admin/
+                        //var con = '<img src="img/upImgLoad.png" class="webchat_img_upload upNowImg">';
+
+                        showMsg(uid,"daijm","img/qqarclist/jianjiao.gif","",null,"<img src='img/appType.png'>",base64file);
+                        //显示气泡
+                    }
+                    //else
+                    //{console.log(2);
+                    //	var oMsgDom = '<div class="systeamTextBox systeamNowText"><p class="systeamText">正在上传 '+ file +'</p></div>';
+                    //	$node.find(".panel-body").append(oMsgDom);
+                    //$node.find('.scrollBoxParent').scrollTop(999999);
+                    //}
+                },
+                onComplete : function(file,response) {
+                    var countTag = 0,
+                        res,
+                        url,
+                        size;
+
+                    if( typeof response == 'string') {
+                        res = JSON.parse(response);
+                        url = res.url;
+                        countTag = parseInt(res.countTag) - 1;
+                    } else {
+                        url = response.url;
+                    }
+
+                    size = response.filesize;
+
+                    if(size == false) {
+                        $.amaran({
+                            content : {
+                                message : '所传文件过大!',
+                                size : '',
+                                file : '',
+                                icon : 'fa fa-times'
+                            },
+                            theme : 'default error',
+                            position : 'bottom right',
+                            inEffect : 'slideRight',
+                            outEffect : 'slideBottom'
+                        });
+                        return;
+                    }
+
+                    //imgCallBack(uid, url, cid, countTag);
+
+                }
+            }
+            /*
+             *uploadBtn 附件按钮
+             *uploadOption 上传参数
+             */
+            new AjaxUpload(uploadBtn,uploadOption);
+        }
+
+    };
     var init = function() {
         parseDOM();
         bindLitener();
@@ -254,4 +259,3 @@ function TextArea(node,core,window){
 }
 
 module.exports = TextArea;
- 
