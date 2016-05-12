c793c6b9ceba9da3c27261e7eacee02e2f05b9a5
GoblinStar <testlushi2@163.com>
2016-05-12 21:31:47 +0800

    update

--- chat/js/scrollcontent/index.js
+++ chat/js/scrollcontent/index.js
@@ -284,7 +284,7 @@ function Content(node,core,window) {
     // 显示其他在线客服列表
     var showAdminList = function(data) {
 
-    }
+    }  
 
     // --------------------------- socket ---------------------------
 
