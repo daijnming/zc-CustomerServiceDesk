f192e323d62ac827c46d8d8aedb414cfb6bf8474
Zhao <zhaoxb@sobot.com>
2016-05-13 12:04:15 +0800

    develop

--- chat/js/rightside/messagesuser/index.js
+++ chat/js/rightside/messagesuser/index.js
@@ -74,6 +74,27 @@ var FastMsgModal = function(node,core,config) {
             $(oRightQuickRight).append(_html);
         });
     };
+
+    var onContentLoaded = function(ret) {
+        var data = ret || [];
+        if(!ret) {
+            $(node).find('.js-rightQuickLeft ul').html('');
+            $(node).find('.js-rightQuickRight ul').html('');
+        }
+
+        loadFile.load(global.baseUrl + "views/rightside/fastreplylayer.html").then(function(value) {
+            var _html = doT.template(value)({
+                'list' : data
+            });
+            outer.dialog = new Dialog({
+                'title' : '快捷回复',
+                'footer' : false
+            });
+            outer.dialog.setInner(_html);
+            outer.shadowNode = outer.dialog.getOuter();
+        });
+    };
+
     //初始化数据
     var initData = function() {
         var promise = new Promise();
@@ -88,24 +109,14 @@ var FastMsgModal = function(node,core,config) {
             dataType : "jsonp",
             success : function(data) {
                 config.fastData = data;
-                if(!data) {
-                    $(node).find('.js-rightQuickLeft ul').html('');
-                    $(node).find('.js-rightQuickRight ul').html('');
-                    return;
-                }
-                loadFile.load(global.baseUrl + "views/rightside/fastreplylayer.html").then(function(value) {
-
-                    var _html = doT.template(value)({
-                        'list' : data
-                    });
-                    outer.dialog = new Dialog({
-                        'title' : '快捷回复',
-                        'footer' : false
-                    });
-                    outer.dialog.setInner(_html);
-                    outer.shadowNode = outer.dialog.getOuter();
-                    promise.resolve();
-                });
+                promise.resolve();
+                onContentLoaded(data);
+            }
+        }).fail(function(ret,err) {
+            if(err === 'parsererror') {
+                config.fastData = [];
+                onContentLoaded();
+                promise.resolve();
             }
         });
         return promise;
--- chat/js/rightside/profileuser/index.js
+++ chat/js/rightside/profileuser/index.js
@@ -1,157 +1,190 @@
-
 /*
-* @author denzel.gou
-*/
+ * @author denzel.gou
+ */
 
 var ProfileUser = function(node,core,userData) {
-	//TODO
-	var global=core.getGlobal();//全局对象
-	var data = userData,
-			config ={};
-	var regExUserInfo=[];//保存用户验证信息
-	//加载模版
-	var loadFile = require('../../util/load.js')();
-	var Promise = require('../../util/promise.js');
+    //TODO
+    var global = core.getGlobal();
+    //全局对象
+    var data = userData,
+        config = {};
+    var regExUserInfo = [];
+    //保存用户验证信息
+    //加载模版
+    var loadFile = require('../../util/load.js')();
+    var Promise = require('../../util/promise.js');
 
-	//TODO 处理对话页显示  此处只是页面显示 不影响页面重构 不需要使用模版
-	var onVisitHandle = function(url,title){
-		var regexUrl = /^(https?)/;
-		if(url){
-			if(!url.match(regexUrl))url='http://'+url;
-		}
-		var subTitle= title&&title.length>15?title.substr(0,15)+'..':title;
-		//暂定成35个字符
-		if(!url&&!title) return '未获取到';
-		if(url&&!title) {
-				var urlTitle = url.length>25?url.substr(0,25)+'..':url;
-				return	'<a target="_black" style="font-size:14px;" href="'+url+'" title="'+url+'">'+urlTitle+'</a>';
-		}
-		if(!url&&title) {
+    //TODO 处理对话页显示  此处只是页面显示 不影响页面重构 不需要使用模版
+    var onVisitHandle = function(url,title) {
+        var regexUrl = /^(https?)/;
+        if(url) {
+            if(!url.match(regexUrl))
+                url = 'http://' + url;
+        }
+        var subTitle = title && title.length > 15 ? title.substr(0,15) + '..' : title;
+        //暂定成35个字符
+        if(!url && !title)
+            return '未获取到';
+        if(url && !title) {
+            var urlTitle = url.length > 25 ? url.substr(0,25) + '..' : url;
+            return '<a target="_black" style="font-size:14px;" href="' + url + '" title="' + url + '">' + urlTitle + '</a>';
+        }
+        if(!url && title) {
 
-				return  subTitle;
-		}
-		return '<a target="_black" style="font-size:14px;" href="'+url+'" title="'+title+'">'+subTitle+'</a>';
+            return subTitle;
+        }
+        return '<a target="_black" style="font-size:14px;" href="' + url + '" title="' + title + '">' + subTitle + '</a>';
 
-	};
+    };
 
-	//初始化页面
-	var initUserInfo = function(){
-			var promise =  new Promise();
-			//有数据再添加dom
-			if(data){
-				//客户资料背景清除
-				$(node).html('').removeClass('showBg');
-				loadFile.load(global.baseUrl+'views/rightside/profileUser.html').then(function(value){
-					//组装对话页
-					// console.log(data);
-					data.userData["visit"] = onVisitHandle(data.userData['visitUrl'],data.userData['visitTitle']);
-					var _html = doT.template(value)({
-							'item':data.userData
-					});
-					$(node).append(_html);
-					//显示性别
-					$(node).find('#sex').val(data.userData['sex']);
-					promise.resolve();
-				});
-				// console.log(data.userData);
-			}
-			return promise;
-	};
+    //初始化页面
+    var initUserInfo = function() {
+        var promise = new Promise();
+        //有数据再添加dom
+        if(data) {
+            //客户资料背景清除
+            $(node).html('').removeClass('showBg');
+            loadFile.load(global.baseUrl + 'views/rightside/profileUser.html').then(function(value) {
+                //组装对话页
+                // console.log(data);
+                data.userData["visit"] = onVisitHandle(data.userData['visitUrl'],data.userData['visitTitle']);
+                var _html = doT.template(value)({
+                    'item' : data.userData
+                });
+                $(node).append(_html);
+                //显示性别
+                $(node).find('#sex').val(data.userData['sex']);
+                promise.resolve();
+            });
+            // console.log(data.userData);
+        }
+        return promise;
+    };
 
-	//保存客户资料字段值
-	var onTextSaveData = function(val,obj){
+    //保存客户资料字段值
+    var onTextSaveData = function(val,obj) {
 
-		var oUrl = 'admin/modify_userinfo.action',
-				sendData = {},
-				type = $(obj).attr('otype');
-				sendData.uid=config.uid;
-				sendData.sender = config.url_id;
-				if(type)sendData[type] = val;
-				$.ajax({
-				type:'post',
-				url:oUrl,
-				data:sendData,
-				dataType:'json',
-				success:function(data){
-					//如果编辑的是姓名字段 则要传值给左侧栏显示
-					if($(obj).hasClass('userNameDyy'))
-					{
-						$(document.body).trigger('rightside.onGetName',[{
-								'name':val
-						}]);
-					}
-				}
-			});
+        var oUrl = 'admin/modify_userinfo.action',
+            sendData = {},
+            type = $(obj).attr('otype');
+        sendData.uid = config.uid;
+        sendData.sender = config.url_id;
+        if(type)
+            sendData[type] = val;
+        $.ajax({
+            type : 'post',
+            url : oUrl,
+            data : sendData,
+            dataType : 'json',
+            success : function(data) {
+                //如果编辑的是姓名字段 则要传值给左侧栏显示
+                if($(obj).hasClass('userNameDyy')) {
+                    $(document.body).trigger('rightside.onGetName',[{
+                        'name' : val
+                    }]);
+                }
+            }
+        });
 
-	};
-	//编辑客户资料字段值
-	var onTextRegexData = function(){
-			var $this =$($(this)[0]);
-			var isSubmit = false;//是否通过验证提交用户资料字段
-			if($this){
-				var val = $this.val().trim().replace(/[ ]/g,'');
-				// console.log(regExUserInfo[$this.attr('otype')].regex);
-				var _cur = regExUserInfo[$this.attr('otype')];
-				(function(_cur,val,$this){
-						if(val==='' || !_cur.regex.test(val)){
-							$this.siblings('span.tip').text(_cur.alert).show();
-							isSubmit = false;
-						}else {
-							$this.siblings('span.tip').text('').hide();
-							isSubmit = true;
-						}
-						//保存
-						if(isSubmit)
-							onTextSaveData(val,$this);
-				})(_cur,val,$this);
-			}
-	};
+    };
+    //编辑客户资料字段值
+    var onTextRegexData = function() {
+        var $this = $($(this)[0]);
+        var isSubmit = false;
+        //是否通过验证提交用户资料字段
+        if($this) {
+            var val = $this.val().trim().replace(/[ ]/g,'');
+            // console.log(regExUserInfo[$this.attr('otype')].regex);
+            var _cur = regExUserInfo[$this.attr('otype')];
+            (function(_cur,val,$this) {
+                if(val === '' || !_cur.regex.test(val)) {
+                    $this.siblings('span.tip').text(_cur.alert).show();
+                    isSubmit = false;
+                } else {
+                    $this.siblings('span.tip').text('').hide();
+                    isSubmit = true;
+                }
+                //保存
+                if(isSubmit)
+                    onTextSaveData(val,$this);
+            })(_cur,val,$this);
+        }
+    };
 
-	var onSelected = function(){
-			var val = $(this).find('option:selected').val()
-			onTextSaveData(val,$(this)[0]);
-	};
-	var parseDOM = function() {
-    // someOne = $(node).find('.js-xx');
-	};
+    var onSelected = function() {
+        var val = $(this).find('option:selected').val()
+        onTextSaveData(val,$(this)[0]);
+    };
+    var parseDOM = function() {
+        // someOne = $(node).find('.js-xx');
+    };
 
-  var onReceive = function(value,data) {
+    var onReceive = function(value,data) {
 
-  };
-	var onloadHandler = function(evt,data) {
+    };
+    var onloadHandler = function(evt,data) {
 
-	};
-	var bindLitener = function() {
-		$(node).delegate('.js-userTnp','blur',onTextRegexData);
-		$(node).delegate('#sex','change',onSelected);
-	};
+    };
+    var bindLitener = function() {
+        $(node).delegate('.js-userTnp','blur',onTextRegexData);
+        $(node).delegate('#sex','change',onSelected);
+    };
 
-	var initConfig = function(){
+    var initConfig = function() {
 
-		config.uid = data.data.uid;//用户id
-		config.url_id = global.id;//url地址栏id
+        config.uid = data.data.uid;
+        //用户id
+        config.url_id = global.id;
+        //url地址栏id
 
-		regExUserInfo = {
-			nick:{'regex':/\w{5,28}/,'alert':'格式错误'},
-			uname:{'regex':/\w{5,28}/,'alert':'格式错误'},
-			tel:{'regex':/^[0-9]{8,13}$/,'alert':'格式错误'},
-			birthday:{'regex':/^[0-9]{2}\-(([0-2]{1}[0-9]{1})|(3[0-1]{1}))(\-([0-2]{1}[0-9]{1})|(3[0-1]{1}))*$/,'alert':'格式错误'},
-			email:{'regex':/^[a-zA-Z0-9]+([-_\.][a-zA-Z0-9]+)*(?:@(?!-))(?:(?:[a-zA-Z0-9]*)(?:[a-zA-Z0-9](?!-))(?:\.(?!-)))+[a-zA-Z]{2,}$/,'alert':'格式错误'},
-			qq:{'regex':/[1-9][0-9]{4,13}/,'alert':'格式错误'},
-			weixin:{'regex':/\w{5,28}/,'alert':'格式错误'},
-			weibo:{'regex':/\w{5,28}/,'alert':'格式错误'},
-			remark:{'regex':/\w{0,200}/,'alert':'格式错误'}
-		};
-	};
+        regExUserInfo = {
+            nick : {
+                'regex' : /\w{5,28}/,
+                'alert' : '格式错误'
+            },
+            uname : {
+                'regex' : /\w{5,28}/,
+                'alert' : '格式错误'
+            },
+            tel : {
+                'regex' : /^[0-9]{8,13}$/,
+                'alert' : '格式错误'
+            },
+            birthday : {
+                'regex' : /^[0-9]{2}\-(([0-2]{1}[0-9]{1})|(3[0-1]{1}))(\-([0-2]{1}[0-9]{1})|(3[0-1]{1}))*$/,
+                'alert' : '格式错误'
+            },
+            email : {
+                'regex' : /^[a-zA-Z0-9]+([-_\.][a-zA-Z0-9]+)*(?:@(?!-))(?:(?:[a-zA-Z0-9]*)(?:[a-zA-Z0-9](?!-))(?:\.(?!-)))+[a-zA-Z]{2,}$/,
+                'alert' : '格式错误'
+            },
+            qq : {
+                'regex' : /[1-9][0-9]{4,13}/,
+                'alert' : '格式错误'
+            },
+            weixin : {
+                'regex' : /\w{5,28}/,
+                'alert' : '格式错误'
+            },
+            weibo : {
+                'regex' : /\w{5,28}/,
+                'alert' : '格式错误'
+            },
+            remark : {
+                'regex' : /\w{0,200}/,
+                'alert' : '格式错误'
+            }
+        };
+    };
 
-	initUserInfo().then(function(){
-		init();
-		//console.log(data);
-	});
-	var init = function() {
-		initConfig();
-		parseDOM();
-		bindLitener();
-	};
+    initUserInfo().then(function() {
+        init();
+        //console.log(data);
+    });
+    var init = function() {
+        initConfig();
+        parseDOM();
+        bindLitener();
+    };
 };
 module.exports = ProfileUser;
