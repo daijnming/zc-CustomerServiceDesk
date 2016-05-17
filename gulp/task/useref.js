ed633492e453ac2f626d9e20e1b78f3c40ba311a
Zhao <zhaoxb@sobot.com>
2016-05-17 18:51:17 +0800

    devlop

--- gulp/task/useref.js
+++ gulp/task/useref.js
@@ -8,7 +8,7 @@ var uglify = require('gulp-uglify');
 var minifyCss = require("gulp-minify-css");
 var autoPrefixed = require("gulp-autoprefixer");
 gulp.task('useref',['browserify'], function() {
-    gulp.src('chat/chat.html').
+   return gulp.src('chat/chat.html').
     pipe(useref()).
     pipe(gulpIf("*.css",minifyCss())).
     pipe(gulpIf("*.css",autoPrefixed())).
