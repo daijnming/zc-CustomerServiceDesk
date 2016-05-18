983b1cd40e397f4672f7d87c9e760d7134b8fc6f
Zhao <zhaoxb@sobot.com>
2016-05-18 16:54:29 +0800

    develop

--- chat/js/core/socket/json.js
+++ chat/js/core/socket/json.js
@@ -24,15 +24,34 @@ function polling(global) {
                 'cid' : data.cid,
                 'uid' : global.id
             })
+        }).success(function() {
+
+        }).fail(function() {
+
         });
     };
 
     var onDirectSend = function(evt,ret) {
+        ret = ret.data;
+        console.log(ret);
+        if(ret.status == 1 || ret.stats == 1) {
+            $.ajax({
+                'url' : '/chat/admin/send1.action',
+                'dataType' : 'json',
+                'type' : "post",
+                'data' : $.extend(defaultParams, {
+                    'answer' : ret.msg,
+                    'cid' : data.cid,
+                    'uid' : global.id
+                })
+            });
+        }
+        alert();
     };
 
     var bindListener = function() {
         $(document.body).on("textarea.send",onsend);
-        $(document.body).on("rightsie.onChatSmartReply",onDirectSend);
+        $(document.body).on("rightside.onChatSmartReply",onDirectSend);
     };
 
     var messageAdapter = function(ret) {
