a91af244f9350cf02dfbf2f2ff2472fd003f3ec5
Zhao <zhaoxb@sobot.com>
2016-05-19 18:28:46 +0800

    develop

--- chat/js/core/socket/json.js
+++ chat/js/core/socket/json.js
@@ -16,7 +16,6 @@ function polling(global) {
     };
 
     var onsend = function(evt,data) {
-        console.log(data.answer);
         var answer = unescape(data.answer.replace("\\u","%u"));
         $.ajax({
             'url' : '/chat/admin/send1.action',
