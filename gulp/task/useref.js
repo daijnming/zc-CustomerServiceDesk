caef1276cacef3a1cdd13a7d62ec0716610d7065
Zhao <zhaoxb@sobot.com>
2016-05-19 10:50:13 +0800

    develop

--- gulp/task/useref.js
+++ gulp/task/useref.js
@@ -7,7 +7,14 @@ var gulpIf = require("gulp-if");
 var uglify = require('gulp-uglify');
 var minifyCss = require("gulp-minify-css");
 var autoPrefixed = require("gulp-autoprefixer");
-gulp.task('useref',['browserify'], function() {
+
+gulp.task('move-file',function(){
+	return gulp.src('./chat/**/*.mp3').
+	pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('useref',['browserify','move-file'], function() {
    return gulp.src('chat/chat.html').
     pipe(useref()).
     pipe(gulpIf("*.css",minifyCss())).
