e0b287cfc7aba2fab37108d19efda87a0d4c6b64
Zhao <zhaoxb@sobot.com>
2016-05-19 11:40:04 +0800

    2016年 5月19日 星期四 11时40分04秒 CST

--- build.sh
+++ build.sh
@@ -7,5 +7,13 @@ rm rev-manifest.json
 git status -s | egrep "^[ ]*D" | sed 's`^[ ]*D``' | xargs git rm
 git status -s | egrep "^[ ]*M" | sed 's`^[ ]*M``' | xargs git add
 git status -s  | egrep "^[ ]*\?" | sed 's`^[ ]*\?*``g' | xargs git add 
-echo "\033[31m===================== 请确认一下文件变化\033[0m"
-echo "\033[31m===================== \033[0m"
+echo "\033[32m===================== 请确认一下文件变化\033[0m"
+git status -s
+echo "\033[32m===================== \033[0m"
+echo "请确认(y/n)"
+read x
+if [ "$x"x = "y"x ];then
+date=$(date);
+git commit -m "$date"
+git push
+fi
