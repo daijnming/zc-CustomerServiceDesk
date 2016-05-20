57537c6bf7f33e0ca5edcda5753ee9955006704d
Zhao <zhaoxb@sobot.com>
2016-05-20 14:10:10 +0800

    2016年 5月20日 星期五 14时10分10秒 CST

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ gulp production
 find chat/img -name "*.mp3" -type f -exec cp {} dist/img  \;
 cp -r chat/assets ./dist
 rm rev-manifest.json
+cp -r chat/views dist
 git status -s | egrep "^[ ]*D" | sed 's`^[ ]*D``' | xargs git rm
 git status -s | egrep "^[ ]*M" | sed 's`^[ ]*M``' | xargs git add
 git status -s  | egrep "^[ ]*\?" | sed 's`^[ ]*\?*``g' | xargs git add 
-cp -r chat/views dist
 echo "\033[32m===================== 请确认一下文件变化\033[0m"
 git status -s
 echo "\033[32m===================== \033[0m"
