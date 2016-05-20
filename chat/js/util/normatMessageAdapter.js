93844f52083e6e271fb21a8399454087c98f3066
Zhao <zhaoxb@sobot.com>
2016-05-20 11:21:51 +0800

    develop

--- chat/js/util/normatMessageAdapter.js
+++ chat/js/util/normatMessageAdapter.js
@@ -3,9 +3,10 @@
  */
 var TYPE_EMOTION = 0,
     TYPE_IMAGE = 1,
-    TYPE_TEXT = 2;
+    TYPE_TEXT = 2,
+    AUDIO = 3,
+    RICH_TEXT = 4;
 var normalMessageAdapter = function(value) {
-    console.log(value);
     var content = value.content || value.answer;
     var reg = /src=['"](.*?)['"]/;
     if(content.indexOf("<img") >= 0) {
@@ -15,11 +16,21 @@ var normalMessageAdapter = function(value) {
         } else if(content.indexOf("webchat_img_upload") >= 0) {
             value.message_type = TYPE_IMAGE;
             value.desc = '[图片]';
+        } else if(content.indexOf("<a")) {
+            console.log("富文本");
+            value.message_type = RICH_TEXT;
+            value.desc = '[富文本]';
         }
         if(reg.test(content)) {
             value.url = RegExp.$1;
         }
     } else if(content.indexOf("<audio") >= 0) {
+        value.message_type = AUDIO;
+        value.desc = '[语音]';
+    } else if(content.indexOf("<a") >= 0) {
+        console.log("富文本");
+        value.message_type = RICH_TEXT;
+        value.desc = '[富文本]';
     } else {
         value.message_type = TYPE_TEXT;
         value.desc = content;
