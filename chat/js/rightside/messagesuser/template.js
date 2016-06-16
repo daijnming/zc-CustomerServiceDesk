a52db612f76d91e5297d5127f7fc25f8c9c607c7
denzel <422293027@qq.com>
2016-06-16 13:48:47 +0800

    develop

--- chat/js/rightside/messagesuser/template.js
+++ chat/js/rightside/messagesuser/template.js
@@ -1,8 +1,8 @@
 var template = {};
 
-var zcReplyOuter = '<li class="js-detalBar detalBar show"><input utype="'
+var zcReplyOuter = '<li class="js-detalBar detalBar show activeLine"><input utype="'
 +'{{=it.utype}}'+
-'" type="text" class="newInput" maxlength={{=it.maxlength}} placeholder="请输入..." ><span class="'
+'" type="text" class="newInput activeLine" maxlength={{=it.maxlength}} placeholder="请输入..." ><span class="'
 +'{{=it.clsDelName}}'+
 '">删除</span><span class="'
 +'{{=it.clsUpName}}'+'" >置顶</span></li>';
