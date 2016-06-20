cf4e2ef8060565e0f49ee32cf8c781350260491f
Zhao <zhaoxb@sobot.com>
2016-06-20 14:27:38 +0800

    Develop

--- chat/js/leftside/chatItem.js
+++ chat/js/leftside/chatItem.js
@@ -397,7 +397,7 @@ function Item(data,core,outer,from,manager) {
     var bindStaticListener = function() {
         $body = $(document.body);
         $body.on("core.receive", function(evt,list) {
-            setTimeout(function() {
+            setTimeout(function(evt,list) {
                 onReceive(evt,list);
             },1);
         });
