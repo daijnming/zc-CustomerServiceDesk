28706d96c0bb61f1d6a11a9e9d97cd3863f5a355
Zhao <zhaoxb@sobot.com>
2016-06-20 11:07:08 +0800

    develop

--- chat/js/core/core.js
+++ chat/js/core/core.js
@@ -252,7 +252,6 @@ function Core(window) {
             socket = new polling(global);
         }
 
-
         socket.on("receive", function(list) {
             if(window.confirm("是否进行消息确认？")) {
                 messageConfirm(list);
