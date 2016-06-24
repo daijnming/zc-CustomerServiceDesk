679aa7e9b96a21d283c585a6a8f2066bf142398d
XiaoDai <15712897291@163.com>
2016-06-24 16:08:04 +0800

    deve

--- chat/js/textarea/template.js
+++ chat/js/textarea/template.js
@@ -1,7 +1,7 @@
 var template = {};
 
 var tranfiletype = '<img style="vertical-align: middle; margin-right: 2px;" class="uploadedFile" src="'+'{{=it.fileIcon}}'+'"><a style="font-size:10px;" target="_blank" href="'+'{{=it.url}}'+'">'+'{{=it.filename}}'+'{{=it.extension}}'+'</a>';
-var fileImage='<a href="' + '{{=it.url}}' + '" target="_blank"><img class="webchat_img_upload upNowImg" src="' + '{{=it.url}}' + '" /></a>';
+var fileImage='<a href="' + '{{=it.url}}' + '" target="_blank"><img class="webchat_img_upload upNowImg uploadedFile" src="' + '{{=it.url}}' + '" /></a>';
 var uploading='<div class="systeamTextBox systeamNowText js-systeamTextBox'+'{{=it.date}}'+'"><p class="systeamText">正在上传 '+'{{=it.filename}}{{=it.extension}}'+'</p></div>';
 
 template.tranfiletype = tranfiletype;
